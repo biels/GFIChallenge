cdd179a6e41f6d11a78c38607263c2714d4384a4
Biel Simon Rojas <bielsimon2@gmail.com>
2016-10-08 21:44:33 +0200

    Created event client

---
+++ app/src/main/java/gfi/gfichallenge/EventClient.java
@@ -0,0 +1,28 @@
+package gfi.gfichallenge;
+
+import org.springframework.web.client.RestTemplate;
+
+import gfi.gfichallenge.entities.Event;
+
+/**
+ * Created by Biel on 8/10/2016.
+ */
+
+public class EventClient {
+    final String localhost = "localhost";
+    final int port = 8080;
+    final String uri = "http://" + localhost + ":" + port + "/event";
+    private Event event;
+    public void refresh(){
+        event = requestEvent();
+    }
+    private Event requestEvent(){
+        RestTemplate restTemplate = new RestTemplate();
+        Event result = restTemplate.getForObject(uri, Event.class);
+        return result;
+    }
+
+    public Event getEvent() {
+        return event;
+    }
+}
