0d1405eac5bc115bf5155ff93b46b89ce09d6929
Biel Simon Rojas <bielsimon2@gmail.com>
2016-10-08 20:09:46 +0200

    Updated event, added getters and setters

--- app/src/main/java/gfi/gfichallenge/entities/Event.java
+++ app/src/main/java/gfi/gfichallenge/entities/Event.java
@@ -1,12 +1,25 @@
 package gfi.gfichallenge.entities;
 
-import java.time.Instant;
-import java.util.Date;
-
 /**
  * Created by Biel on 8/10/2016.
  */
 public class Event {
     Animation  animation;
     Long startIntant;
+
+    public Animation getAnimation() {
+        return animation;
+    }
+
+    public void setAnimation(Animation animation) {
+        this.animation = animation;
+    }
+
+    public Long getStartIntant() {
+        return startIntant;
+    }
+
+    public void setStartIntant(Long startIntant) {
+        this.startIntant = startIntant;
+    }
 }
