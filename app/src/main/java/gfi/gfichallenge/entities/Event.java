ba3df9500b1df0f9c25b1686ee6600dfef332c31
Biel Simon Rojas <bielsimon2@gmail.com>
2016-10-09 01:39:10 +0200

    Added UUID on client

--- app/src/main/java/gfi/gfichallenge/entities/Event.java
+++ app/src/main/java/gfi/gfichallenge/entities/Event.java
@@ -1,12 +1,14 @@
 package gfi.gfichallenge.entities;
 
+import java.util.UUID;
+
 /**
  * Created by Biel on 8/10/2016.
  */
 public class Event {
     Animation  animation;
     Long timeToStart;
-
+    UUID uuid;
     public Animation getAnimation() {
         return animation;
     }
@@ -21,5 +23,13 @@ public class Event {
 
     public void setTimeToStart(Long timeToStart) {
         this.timeToStart = timeToStart;
+    }
+
+    public UUID getUuid() {
+        return uuid;
+    }
+
+    public void setUuid(UUID uuid) {
+        this.uuid = uuid;
     }
 }
